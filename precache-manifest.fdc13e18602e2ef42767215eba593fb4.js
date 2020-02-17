349195109f50cd470aeb64aaa1a01663e614ca0a
Dylan <dylanlarrivee@gmail.com>
2020-02-17 15:14:00 -0800

    Updates

---
+++ precache-manifest.fdc13e18602e2ef42767215eba593fb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4523a6f6305d1a1c6f74d8594a2f32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be64c058e48da07795f7",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "80aec388295e21ad6bed",
+    "url": "/static/css/main.0ffdaefc.chunk.css"
+  },
+  {
+    "revision": "be64c058e48da07795f7",
+    "url": "/static/js/2.e43258f5.chunk.js"
+  },
+  {
+    "revision": "8956eb797ec1ca27a54b67665871977c",
+    "url": "/static/js/2.e43258f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80aec388295e21ad6bed",
+    "url": "/static/js/main.f2d0589e.chunk.js"
+  },
+  {
+    "revision": "abaf275cf00a7d306389",
+    "url": "/static/js/runtime-main.5aba977e.js"
+  },
+  {
+    "revision": "7aed99496792194ca42810ad48cee0a2",
+    "url": "/static/media/ri-program-stopper.7aed9949.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dlarrivee.github.io/precache-manifest.e2ceb9e1789b502ee53ac1c838d5a434.js"
+  "/precache-manifest.fdc13e18602e2ef42767215eba593fb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dlarrivee.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
